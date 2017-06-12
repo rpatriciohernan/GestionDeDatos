dccb5b17ee354ce1844252ec77ed6bbc2a928af7
Patricio Hernán Rodriguez <rodriguez.ph@live.com>
2017-06-12 04:01:08 -0300

    Validacion de formulario de cliente e incorporacion de checkbox para filtrar por estado activo

--- Abm Cliente/Cliente.cs
+++ Abm Cliente/Cliente.cs
@@ -19,7 +19,6 @@ namespace UberFrba.Abm_Cliente
         private String localidad;
         private DateTime fechaNacimiento;
         private String estado;
-        private List<CampoYValor> camposObligatorios;
         private static RepositorioCliente repositorioCliente = RepositorioCliente.Instance;
         #endregion
 
@@ -70,39 +69,26 @@ namespace UberFrba.Abm_Cliente
         #region constructor
         public Cliente(String nombre, String apellido, Int64 dni, String mail, String telefono,
             String domicilio, Int16 codigoPostal, String localidad, DateTime fechaNacimiento, String estado)
-        {
-            //--cargar en esta lista, los campos obligatorios del cliente--
-            this.camposObligatorios = new List<CampoYValor>();
-            
+        {            
             this.nombre = nombre;
-            this.camposObligatorios.Add(new CampoYValor("Nombre", this.nombre));
 
             this.apellido = apellido;
-            this.camposObligatorios.Add(new CampoYValor("Apellido", this.apellido));
             
             this.dni = dni;
-            this.camposObligatorios.Add(new CampoYValor("Dni", this.dni.ToString()));
            
             this.mail = mail;
-            this.camposObligatorios.Add(new CampoYValor("Mail", this.mail));
            
             this.telefono = telefono;
-            this.camposObligatorios.Add(new CampoYValor("Telefono", this.telefono));
 
             this.domicilio = domicilio;
-            this.camposObligatorios.Add(new CampoYValor("Domicilio", this.domicilio));
             
             this.localidad = localidad;
-            this.camposObligatorios.Add(new CampoYValor("Localidad", this.localidad));
   
             this.codigoPostal = codigoPostal;
-            this.camposObligatorios.Add(new CampoYValor("CodigoPostal", this.codigoPostal.ToString()));
             
             this.fechaNacimiento = fechaNacimiento;
-            this.camposObligatorios.Add(new CampoYValor("FechaNacimiento", Convert.ToString(this.fechaNacimiento)));
 
             this.estado = estado;
-            this.camposObligatorios.Add(new CampoYValor("Estado", this.estado));
         
         }
         #endregion
@@ -116,14 +102,6 @@ namespace UberFrba.Abm_Cliente
         //metodos de instancia
         public void guardate() {
             repositorioCliente.Guardar(this);
-        }
-
-        public List<ErrorDeCampo> validarCampos() { //controlar que el nombre del campo sea igual al que conoce el form pq sino no funciona
-            //filter
-            List<CampoYValor> camposObligatoriosVacios = camposObligatorios.Where(elem => (elem.Valor == null || elem.Valor == "")).ToList();
-            //map
-            List<ErrorDeCampo> errores = camposObligatoriosVacios.Select(error => new ErrorDeCampo(error.Campo,"falta completar")).ToList();
-            return errores;
         }
 
         public String GetValues()
