8dc3e24244bd97c3dff93c7fe8dfddb9678cf360
Patricio Hernán Rodriguez <rodriguez.ph@live.com>
2017-06-12 05:00:13 -0300

    validacion en ABM vehiculos y filtro de estado

--- Abm Automovil/FRMAutomovil.designer.cs
+++ Abm Automovil/FRMAutomovil.designer.cs
@@ -51,11 +51,16 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.CMBChofer = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.TXTmodelo);
             this.groupBox1.Controls.Add(this.BTNnuevaMarca);
             this.groupBox1.Controls.Add(this.CMBturno);
@@ -225,7 +230,7 @@
             this.label1.Location = new System.Drawing.Point(-1, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(318, 45);
+            this.label1.Size = new System.Drawing.Size(320, 45);
             this.label1.TabIndex = 20;
             this.label1.Text = "Gestion de Vehiculos";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -308,11 +313,48 @@
             this.CMBChofer.Size = new System.Drawing.Size(364, 21);
             this.CMBChofer.TabIndex = 34;
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(355, 28);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 18);
+            this.label8.TabIndex = 35;
+            this.label8.Text = "*";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(521, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(14, 18);
+            this.label9.TabIndex = 36;
+            this.label9.Text = "*";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(62, 160);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(14, 18);
+            this.label10.TabIndex = 37;
+            this.label10.Text = "*";
+            // 
             // FRMAutomovil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(581, 261);
+            this.Controls.Add(this.label10);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.CMBChofer);
             this.Controls.Add(this.label5);
@@ -354,5 +396,8 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.ComboBox CMBChofer;
         private System.Windows.Forms.TextBox TXTmodelo;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
     }
 }
